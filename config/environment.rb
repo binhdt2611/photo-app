62b297bde54531cd09b905a71450aef247f95044
Binh Do <binh@worldfront.co>
2023-04-22 11:01:09 +1200

    Setup Email in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# We don't have our smtp server, so should update these when we have
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.server.com', 
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['EMAIL_USER_NAME'],
+  :password => ENV['EMAIL_PASSWD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Configure action mailer - Enable in production - but we don't have production yet so disable temporary
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'custom.domain.com', :protocal => 'https' }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
