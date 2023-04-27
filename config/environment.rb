87ed8568942e67d80caaf0acd7f04aac0739dd3d
Binh Do <binh@worldfront.co>
2023-04-28 07:48:12 +1200

    Update SMTP server info

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 # We don't have our smtp server, so should update these when we have
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.server.com', 
+  :address => 'smtp.sendgrid.net', 
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['EMAIL_USER_NAME'],
-  :password => ENV['EMAIL_PASSWD'],
-  :domain => 'heroku.com',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'railway.app',
   :enable_starttls_auto => true
 }
